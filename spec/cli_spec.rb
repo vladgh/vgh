c9cf5298a20afffd440ac381e5b4a1e6643a931b
Vlad <vg@vladgh.com>
2013-03-01 18:14:46 -0600

    Fix cli tests
    Signed-off-by: Vlad <vg@vladgh.com>

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -6,31 +6,32 @@ require 'vgh/cli'
 describe VGH::CLI do
 
   let(:app) {VGH::APPS.list.shuffle.first}
+  let(:cli) {VGH::CLI.new.options}
 
-  before(:all) do
+  before(:each) do
     ARGV[0] = app
   end
 
   it 'Should get the app name as the first argument' do
-      subject.options[:app].should eq app
-  end
-
-  it 'Verbosity' do
-    subject.options[:verbose].should be_false
+    cli[:app].should eq app
   end
 
   it 'No Verbosity' do
+    cli[:verbose].should be_false
+  end
+
+  it 'Verbosity' do
     ARGV.push('--verbose')
-    subject.options[:verbose].should be_true
+    cli[:verbose].should be_true
   end
 
   it 'Logging' do
     ARGV.push('--logging')
-    subject.options[:logging].should be_true
+    cli[:logging].should be_true
   end
 
   it 'No Logging' do
-    subject.options[:logging].should be_false
+    cli[:logging].should be_false
   end
 
 end
